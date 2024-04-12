dfb4c28d92bbdfb98edeeb9f0e6623cd0040404d
rafascript <rafa.eel1@live.com>
2024-04-11 21:41:33 -0300

    Testes automatizados + JPQL adicionados para Tutor

--- src/main/java/app/repository/TutorRepository.java
+++ src/main/java/app/repository/TutorRepository.java
@@ -1,9 +1,28 @@
 package app.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import app.entity.Tutor;
 
 public interface TutorRepository extends JpaRepository<Tutor, Long>{
+	
+	public List<Tutor> findByNome (String nome);
+	
+	public List<Tutor> findByCpf (String cpf);
+	
+	public List<Tutor> findByEndereco (String endereco);
+	
+	public List<Tutor> findByPacienteNome (String nome);
+	
+	// -----JPQL-----
+	
+	@Query("SELECT t FROM Tutor t WHERE t.nome LIKE CONCAT ('%', :nome, '%')")
+	public List<Tutor> findByTrechoNome(String nome);
+	
+	@Query("SELECT t FROM Tutor t WHERE t.cpf LIKE CONCAT ('%', :cpf, '%')")
+	public List<Tutor> findByTrechoCpf(String cpf);
 
 }
--- src/main/java/app/service/TutorService.java
+++ src/main/java/app/service/TutorService.java
@@ -11,10 +11,10 @@ import app.repository.TutorRepository;
 @Service
 public class TutorService {
 	@Autowired
-	private TutorRepository tutorRepositry;
+	private TutorRepository tutorRepository;
 	
 	public String save(Tutor tutor) {
-		this.tutorRepositry.save(tutor);
+		this.tutorRepository.save(tutor);
 		return "Tutor(a) "+tutor.getNome() + " cadastrado com sucesso!";
 	}
 	
@@ -22,7 +22,7 @@ public class TutorService {
 	//fazer validaçao se o id existe ou não
 	public String update(long id, Tutor tutor) {
 		tutor.setId(id);
-		this.tutorRepositry.save(tutor);
+		this.tutorRepository.save(tutor);
 		return "Cadastro do tutor(a) " +tutor.getNome() + " alterado com sucesso!";
 		
 		/*if(id < 0) {
@@ -39,19 +39,45 @@ public class TutorService {
 		if(id < 0) {
 			throw new RuntimeException("ID inválido. O ID deve ser maior que 0.");
 		}else {
-			this.tutorRepositry.deleteById(id);
+			this.tutorRepository.deleteById(id);
 			return "Cadastro do tutor(a) deletado com sucesso!";
 		}
 	}
 	
 	//ver se tem como fazer uma verificação para dar mensagem de lista nula
 	public List<Tutor>listAll(){
-		return this.tutorRepositry.findAll();
+		return this.tutorRepository.findAll();
 	}
 	
 	public Tutor findById(long id) {
-		Tutor tutor = this.tutorRepositry.findById(id).get();
+		Tutor tutor = this.tutorRepository.findById(id).get();
 		return tutor;
+	}
+	
+	public List<Tutor> findByNome (String nome) {
+		return this.tutorRepository.findByNome(nome);
+	}
+	
+	public List<Tutor> findByCpf (String cpf){
+		return this.tutorRepository.findByCpf(cpf);
+	}
+	
+	public List<Tutor> findByEndereco (String endereco){
+		return this.tutorRepository.findByEndereco(endereco);
+	}
+	
+	public List<Tutor> findByPacienteNome (String nome){
+		return this.tutorRepository.findByPacienteNome(nome);
+	}
+	
+	// NOME QUE CONTENHA
+	public List<Tutor> findByTrechoNome(String nome) {
+		return this.tutorRepository.findByTrechoNome(nome);
+	}
+	
+	// CPF QUE CONTENHA
+	public List<Tutor> findByTrechoCpf(String cpf) {
+		return this.tutorRepository.findByTrechoNome(cpf);
 	}
 
 }
