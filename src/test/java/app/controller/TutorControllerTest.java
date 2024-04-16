a0c58c41829d85b649a25b0208385b8d3e4a8714
rorxtto <pedrohenriqueroratto@gmail.com>
2024-04-15 21:04:16 -0300

    adicioando os teste de integracao exception e sucess para o metodo delete

--- src/test/java/app/controller/TutorControllerTest.java
+++ src/test/java/app/controller/TutorControllerTest.java
@@ -1,5 +1,36 @@
 package app.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class TutorControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+
+    @Test
+    public void testDelete() throws Exception {
+        // Execute a solicitação DELETE e espera o status HTTP 200 (OK)
+        mockMvc.perform(delete("/api/tutor/delete/{id}", 1L)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+    
+    @Test
+    public void testDeleteBadRequest() throws Exception {
+        // Tenta excluir um paciente com ID inválido (por exemplo, -1)
+        mockMvc.perform(delete("/api/tutor/delete/{id}", -1L) // ID inválido
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest()); // Espera-se um Bad Request (400)
+    }
 
 }
